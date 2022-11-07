b8c8ea4b5a1f00fff2e8f2f161ea974681c09f90
sidda <siddannadaddannavar@gmail.com>
2022-11-07 08:45:01 +0530

    booleanvalue integervalue

---
+++ Desktop/NEW PROJECT/code.py
@@ -0,0 +1,14 @@
+booleanValue = True
+print(booleanValue)
+
+integerValue =10
+print (type (integerValue))
+
+integerValue, floatValue = 10, 10.5
+stringValue, booleanValue = "pes", True
+print(integerValue, floatValue, stringValue, booleanValue)
+
+integerValue = 51; print(integerValue)
+
+inputValue = input("Enter a value: ")
+print(inputValue)
