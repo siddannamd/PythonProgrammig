1878b796e0663f51c9e4f84e8310b9a4ed93f510
sidda <siddannadaddannavar@gmail.com>
2022-11-07 08:48:34 +0530

    student

---
+++ Desktop/NEW PROJECT/student.py
@@ -0,0 +1,7 @@
+studentUSN = input("Enter a USN: ")
+studentname= input("Enter name of the student: ")
+studentmarks= input("Enter a marks: ")
+print("Student USN: ", studentUSN)
+print("Student NAME: " , studentname)
+print("student marks: ", studentmarks)
+
