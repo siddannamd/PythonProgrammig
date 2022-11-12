e4388298fcb14fb5daa19b8a7a7465d4857cd858
sidda <siddannadaddannavar@gmail.com>
2022-11-12 11:12:45 +0530

    Simple calculator

--- Desktop/PYTHON PROJECT/Scientific_calculator.py
+++ Desktop/PYTHON PROJECT/Scientific_calculator.py
@@ -1,23 +1,57 @@
-root = Tk()
+# Program make a simple calculator
 
-# sets the name on the top of the gui
-root.title("Scientific Calculator")
+# This function adds two numbers
+def add(x, y):
+    return x + y
 
-# sets the background color of the calculator
-# as white
-root.configure(background = 'white')
 
-# fixed the width and height of the gui,
-# hence can't be expanded/stretched
-root.resizable(width=False, height=False)
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
 
-# sets the geometry
-root.geometry("480x568+450+90")
 
-# holds the buttons in the calculator,
-# act as a container for numbers and operators
-calc = Frame(root)
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
 
-# create a grid like pattern of the frame
-# i.e buttons
-calc.grid()
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
+
+    else:
+        print("Invalid Input")
